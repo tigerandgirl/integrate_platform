f91ec73447431a1742e0d96fc371687ba620d7bf
qiufh <qiufh@yonyou.com>
2018-03-12 07:37:36 +0800

    添加 home

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import {Router, Route, Link, IndexRoute, hashHistory} from 'react-router';
+import {Router, Route, Link, IndexRoute, IndexRedirect, hashHistory} from 'react-router';
 
 import $ from 'jquery';
 import App from '@/containers/App';
 import Bundle from '@/bundle.js';
 
+import LoginContainer from "bundle-loader?lazy&name=app-[name]!@/containers/login";
 import ChannelContainer from "bundle-loader?lazy&name=app-[name]!@/containers/channel_manage/Channel";
 import AgencyContainer  from 'bundle-loader?lazy&name=app-[name]!@/containers/agency/Admin';
+
+const Login = (props) => (<Bundle load={LoginContainer}  {...props}>{ (Page) => <Page {...props} />}</Bundle>)
 const Channel = (props) => (<Bundle load={ChannelContainer}  {...props}>{ (Page) => <Page {...props} />}</Bundle>)
 const Agency  = (props) => (<Bundle load={AgencyContainer}   {...props}>{ (Page) => <Page {...props} />}</Bundle>)
 
@@ -15,6 +18,8 @@ export default class RouteConfig extends Component{
     return(
       <Router history={hashHistory}>
         <Route path="/" component={App}>
+          <IndexRedirect to={"login"} />
+          <Route path="/login" component={Login} />
           <Route path="/channel"  component={Channel} />
           <Route path="agency" component={Agency} />
         </Route>
